ced18067f6dbb493c39b19e4dcc065ed1061ad0b
dodyharianto <hariantodody14@gmail.com>
2026-03-21 17:49:20 +0800

    change frontend dependencies and update requirements.txt

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6b1582463ba3efef"; }
+/******/ 		__webpack_require__.h = function() { return "11c9e55573e906ba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
